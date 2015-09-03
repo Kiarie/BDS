3603b0c5fa7f799b687fe58c72e3e8fded87eb5b
Kiarie <Cisco.ku.ac.ke>
2015-09-03 17:55:20 +0300

    added db

---
+++ bin/db_connect.php
@@ -0,0 +1,8 @@
+<?php
+	$user ='root';
+	$pass = '';
+	$db = "message";
+ 
+ 	$mysqli = new mysql_connect("localhost", $user, $pass, $db);
+ 	
+?>
--- bin/message.php
+++ bin/message.php
@@ -1,7 +1,21 @@
 <?php
 if(isset($_POST['projectType']) && isset($_POST['email']) && isset($_POST['name'])){
-	
+	$to= 'operations@bracketDigital.net';
+	$from = mysql_real_escape_string($_POST['email']);
+	$name = htmlspecialchars($_POST['name']);
+	$msg = mysql_real_escape_string($_POST['projectType']);
+	$subject ='Projects';
+	$from = preg_match(/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/, $from);
 
+
+	$headers = 'From: '.$from .'/r/n'.
+	'Reply-To:'. $from. '/r/n'.
+	'X-mailer: PHP/'. phpversion();
+
+	mail($to, $subject, $msg, $headers);
+
+}else{
+	die("Wrong format");
 }
 
 
