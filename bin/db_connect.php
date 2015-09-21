83ded58f724e2295a6ff35d3a32aeb9e76339434
Kiarie <Cisco6@ku.ac.ke>
2015-09-21 17:23:15 +0300

    test for full deployment

--- bin/db_connect.php
+++ bin/db_connect.php
@@ -1,8 +1,20 @@
 <?php
-	$user ='root';
-	$pass = '';
-	$db = "message";
- 
- 	$mysqli = new mysql_connect("localhost", $user, $pass, $db);
- 	
-?>
+	$db = new Sqlite3('messages');
+
+   $sql =<<<EOF
+      CREATE TABLE COMPANY
+      (ID INT PRIMARY KEY     NOT NULL,
+      NAME           TEXT    NOT NULL,
+      EMAIL            INT     NOT NULL,
+      SUBJECT        TEXT      NOT NULL,
+      MSGBODY         TEXT 		NOT NULL);
+EOF;
+
+	$stmt = $db->exec($sql);
+ 	if(!$stmt){
+ 		echo $db->lastErrorMsg();
+ 	} else {
+     	echo "created successfully";
+ 	}
+ 	$db->close();
+ ?>
--- bin/message.php
+++ bin/message.php
@@ -1,4 +1,5 @@
 <?php
+/*
 if(isset($_POST['projectType']) && isset($_POST['email']) && isset($_POST['name'])){
 	$to= 'operations@bracketDigital.net';
 	$from = mysql_real_escape_string($_POST['email']);
@@ -17,7 +18,44 @@ if(isset($_POST['projectType']) && isset($_POST['email']) && isset($_POST['name'
 }else{
 	die("Wrong format");
 }
+*/
+
+if(isset($_POST['projectType']) && isset($_POST['email']) && isset($_POST['name'])){
+	$to= 'operations@bracketDigital.net';
+	$from = htmlspecialchars($_POST['email']);
+	$name = htmlspecialchars($_POST['name']);
+	$msg = htmlspecialchars($_POST['projectType']);
+	$subject ='Projects';
+	$froma= preg_match("/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/", $from);
+
+/**
+* 
+*/
+class Mydb extends Sqlite3
+{
+	
+	function __construct()
+	{
+		# code...
+		$this->open('messages');
+	}
+}
+
+	$db = new Mydb();
+
+   $sql =<<<TOTO
+   INSERT INTO COMPANY(NAME, EMAIL, SUBJECT, MSGBODY) 
+   VALUES ($name, $froma, $subject, $msg);
+TOTO;
 
 
-
+	$ref = $db->exec($sql);
+	
+	if(!$ref){
+		$db->lastErrorMsg();
+		} else {
+		echo "Records inserted !!\n";
+		}
+	$db->close();
+}
 ?>
